974c0f4697e02a9ba7df0681484d032ef288174c
warreir <warrier@gmail.com>
2021-02-26 21:57:09 -0300

    Msg sexta

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,51 +21,59 @@ public class MainActivity extends AppCompatActivity {
         Button btDiv = (Button) findViewById(R.id.btDiv);
         Button btMul = (Button) findViewById(R.id.btMult);
 
-        TextView tFirstNum = (TextView) findViewById(R.id.tFirstNum);
-        TextView tSecondNum = (TextView) findViewById(R.id.tSecondNum);
-
-        fNum = Integer.parseInt(tFirstNum.getText().toString());
-        sNum = Integer.parseInt(tSecondNum.getText().toString());
-
         btSom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText tFirstNum = (EditText) findViewById(R.id.tFirstNum);
+                EditText tSecondNum = (EditText) findViewById(R.id.tSecondNum);
+                fNum = Integer.parseInt(tFirstNum.getText().toString());
+                sNum = Integer.parseInt(tSecondNum.getText().toString());
                 conta(fNum, sNum, 1);
             }
         });
         btSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText tFirstNum = (EditText) findViewById(R.id.tFirstNum);
+                EditText tSecondNum = (EditText) findViewById(R.id.tSecondNum);
+                fNum = Integer.parseInt(tFirstNum.getText().toString());
+                sNum = Integer.parseInt(tSecondNum.getText().toString());
                 conta(fNum, sNum, 2);
             }
         });
         btDiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText tFirstNum = (EditText) findViewById(R.id.tFirstNum);
+                EditText tSecondNum = (EditText) findViewById(R.id.tSecondNum);
+                fNum = Integer.parseInt(tFirstNum.getText().toString());
+                sNum = Integer.parseInt(tSecondNum.getText().toString());
                 conta(fNum, sNum, 3);
             }
         });
         btMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText tFirstNum = (EditText) findViewById(R.id.tFirstNum);
+                EditText tSecondNum = (EditText) findViewById(R.id.tSecondNum);
+                fNum = Integer.parseInt(tFirstNum.getText().toString());
+                sNum = Integer.parseInt(tSecondNum.getText().toString());
                 conta(fNum, sNum, 4);
             }
         });
-
-        Log.v(CATEGORIA, "\nTeste");
     }
 
     private void conta(int fNum, int sNum, int sinal) {
-        TextView res = (TextView) findViewById(R.id.resultado);
-
+        TextView res = (TextView) findViewById(R.id.result);
+        int teste = fNum + sNum;
         if (sinal == 1) {
-            res.setText(fNum+sNum);
+            res.setText('a');
         } else if (sinal == 2) {
-            res.setText(fNum-sNum);
+            res.setText('a');
         } else if (sinal == 3) {
-            res.setText(fNum/sNum);
+            res.setText('a');
         } else {
-            res.setText(fNum*sNum);
+            res.setText('a');
         }
     }
 }
